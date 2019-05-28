120e22525f8b26238f67bf0aea17d23aa0eaf9ed
Amanda Vieira Pires <amandoca_779@hotmail.com>
2019-05-28 00:48:07 -0300

    Inserindo o DML de inserção de testes

---
+++ insert-tests.sql
@@ -0,0 +1,20 @@
+-- DML do projeto MindsY
+-- Inserindo testes
+
+USE MINDSY;
+
+-- INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Cubos', 'Testes com cubos', 69.00, 'executivo');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Conceitos Figurativos', 'Testes com Conceitos Figurativos', 28.00, 'executivo');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Aritmética', 'Testes com Aritmética', 30.00, 'executivo');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Dígitos', 'Testes com Dígitos', 30.00, 'executivo');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Sequência de Números e Letras', 'Testes com Sequência de Números e Letras', 30.00, 'executivo');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Códigos', 'Testes com Códigos', 119.00, 'executivo');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Procurar Símbolos', 'Testes com Procurar Símbolos', 45.00, 'executivo');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Raciocínio Matricial', 'Testes com Raciocínio Matricial', 22.00, 'executivo');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Completar Figuras', 'Testes com Completar Figuras', 0.00, 'executivo');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Cancelamento', 'Testes com Cancelamento', 00.00, 'executivo');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Vocabulário', 'Testes com Vocabulário', 60.00, 'verbal');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Semelhanças', 'Testes com Semelhanças', 33.00, 'verbal');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Compreensão', 'Testes com Compreensão', 36.00, 'verbal');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Informação', 'Testes com Informação', 33.00, 'verbal');
+INSERT INTO TEST (name, description, maximum_score, type) VALUES ('Raciocínio com Palavras', 'Testes com Raciocínio com Palavras', 00.00, 'verbal');
